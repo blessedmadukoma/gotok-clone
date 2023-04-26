023cd30e3f38498d531a16c44c57d3d92fbc62ed
blessedmadukoma <blessedmadukoma@gmail.com>
2023-04-26 15:25:18 +0100

    prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,26 @@ export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     // linear-gradient(90deg,#fee1a7 1px,transparent 0),linear-gradient(180deg,#fee1a7 1px,transparent 0);
-    <main className="bg-[#feebc7] min-h-screen">
-      <div className="space-y-10 flex flex-col items-center">
+    <main className="min-h-screen bg-[#feebc7]">
+      <div className="flex flex-col items-center space-y-10">
         <Header />
 
-        <section className="font-text font-medium text-sm">
-          <button className="py-1 flex text-center items-center justify-between rounded-full w-full bg-gradient-to-r from-[#6670f529] from-90deg to-[#fde3d8bf] to-70.02%">
+        <section className="font-text text-sm font-medium">
+          <button className="from-90deg to-70.02% flex w-full items-center justify-between rounded-full bg-gradient-to-r from-[#6670f529] to-[#fde3d8bf] py-1 text-center">
             <span className="mx-1 ml-2 tracking-wider">
               Refer a friend & Get 25%
             </span>
-            <span className="rounded-full bg-[#f8dacf] ml-2 mr-1 p-1 px-2 items-center text-center">
+            <span className="ml-2 mr-1 items-center rounded-full bg-[#f8dacf] p-1 px-2 text-center">
               →
             </span>
           </button>
         </section>
         <section className="space-y-6 text-center">
-          <h2 className="font-labilgroteskMedium text-7xl text-center max-w-[860px] leading-[1.2]">
+          <h2 className="max-w-[860px] text-center font-labilgroteskMedium text-7xl leading-[1.2]">
             Smart International Payments
           </h2>
 
-          <h4 className="font-text tracking-widest text-2xl flex-wrap text-center">
+          <h4 className="flex-wrap text-center font-text text-2xl tracking-widest">
             Spend, manage & pay bills worldwide with Gotok virtual cards
           </h4>
         </section>
@@ -45,8 +45,8 @@ export default function Home() {
           </a>
         </section>
 
-        <section className="bg-primary-purple w-full rounded-t-[40px] text-white pt-4 text-xl overflow-x-hidden font-labilgroteskMedium">
-          <section className="flex justify-evenly space-x-12 mb-6 mx-6 animate-marquee whitespace-nowrap">
+        <section className="w-full overflow-x-hidden rounded-t-[40px] bg-primary-purple pt-4 font-labilgroteskMedium text-xl text-white">
+          <section className="mx-6 mb-6 flex animate-marquee justify-evenly space-x-12 whitespace-nowrap">
             <span className="flex items-center space-x-1">
               <img src="./check-verified.svg" alt="Verified" />
               <span>Pay bills with Gotok ✨</span>
@@ -65,8 +65,8 @@ export default function Home() {
             </span>
           </section>
 
-          <section className="w-full bg-white rounded-t-[40px] text-center text-4xl min-h-screen text-[#888682]">
-            <section className="pt-8 px-12 text-left space-y-4">
+          <section className="min-h-screen w-full rounded-t-[40px] bg-[#fff9eb] px-40 text-center text-5xl text-[#888682]">
+            <section className="space-y-4 pt-8 text-left">
               <h4 className="tracking-wide">
                 Create a{" "}
                 <span className="text-primary-purple">
@@ -80,6 +80,20 @@ export default function Home() {
                   on Amazon, PayPal, Netflix and anywhere online 🤯 🤩
                 </span>
               </h4>
+            </section>
+
+            <section className="mt-20 grid grid-cols-5 gap-10 text-left text-2xl">
+              <section className="col-span-3 m-4 rounded-3xl bg-white">
+                <h1 className="w-[200px]">
+                  <span>Flexible </span>Funding Options
+                </h1>
+                <span className="w-[200px]">
+                  Funding ease at its best with gotok with multiple options
+                  allowing users flexibility when it comes to financing their
+                  purchases.
+                </span>
+              </section>
+              <section className="col-span-2 rounded-3xl bg-white">2</section>
             </section>
           </section>
         </section>
